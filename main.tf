477d940e554d17ae87c82dfa48dd1fdd095bda21
kartik kalia <kartik_kalia@icloud.com>
2023-10-18 19:31:48 -0400

    update

--- main.tf
+++ main.tf
@@ -3,13 +3,3 @@ provider "aws" {
   region= "us-east-1"
   profile = "KartikK"
 }
-
-# stores the terraform state file in s3
-terraform {
-  backend "s3" {
-    bucket = "kk-s3-tf"
-    key ="kartik.pem"
-    region="us-east-1"
-    profile = "Kartikk"
-      }
-}
